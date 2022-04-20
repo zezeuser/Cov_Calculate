baefaf62873fec73febd9918f794b60cf66cfb8f
zezeuser <1206865201@qq.com>
2022-04-20 19:44:11 +0800

    cov_calculate

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zeze/Cov_Calculate
-# Build directory: /home/zeze/Cov_Calculate/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
